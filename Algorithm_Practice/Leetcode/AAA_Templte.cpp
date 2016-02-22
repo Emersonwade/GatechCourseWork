4b61ad0b57b441a3b3bfc000df5549bc01886068
Shujun Bian <emerson931216@gmail.com>
2016-02-21 22:45:11 -0500

    Internet protocol HW1

--- Algorithm_Practice/Leetcode/AAA_Templte.cpp
+++ Algorithm_Practice/Leetcode/AAA_Templte.cpp
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 using namespace std;
 
 /*
@@ -14,6 +13,35 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {   
+    string s = "s    ";
+    vector<string> result;
+    string resultStr = "";
+    string oneStr = "";
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ' ') {
+            oneStr += s[i];
+        } else {
+            if (oneStr != "") {
+                result.push_back(oneStr);
+            }            
+            oneStr = "";
+        }
+    }
+    if (oneStr != "") {
+        result.push_back(oneStr);
+    }
+
+    for (int i = result.size() - 1; i >= 0; i--) {
+        //cout << result[i] << endl;
+        resultStr += result[i];
+        if (i != 0) {
+            resultStr += " ";
+        }
+    }
+
+    //cout << resultStr << endl;
+    s = resultStr;
+    cout << s << endl;
     system( "read -n 1 -s -p \"Press any key to continue...\"" );
     return 0;
 }
